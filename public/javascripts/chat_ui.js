a8094217e7763fb9bdb566115e96f485e7c8dd16
App Academy Student <student@appacademy.io>
2014-09-22 16:29:37 -0700

    Add user list.

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -23,13 +23,19 @@ var parseText = function (event) {
 
 var socket = io('http://localhost');
 socket.on('addChatMessage', function (message, nickname) {
-	var $ul = $('ul');
-  console.log(message);
+	var $ul = $('.messages-list');
   $ul.prepend('<li>' + nickname + ": " + message + '</li>');
 });
 
 socket.on('systemMessage', function (message) {
-	var $ul = $('ul');
-  console.log(message);
+	var $ul = $('.messages-list');
   $ul.prepend('<li>' + message + '</li>');
 });
+
+socket.on("userList", function (usernames) {
+	var $ul = $(".users-list");
+	$ul.empty();
+	usernames.forEach(function (username) {
+		$ul.append("<li>" + username + "</li>");
+	});
+})
